3ef8e617b4e95e046f744ac10f4cdda6dc7f7ce9
unknown <vulong.pham26@gmail.com>
2022-04-27 13:30:06 -0400

    added service-worker.js / manifest.webmanifest / added paths in index.html

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -16,8 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+{
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
