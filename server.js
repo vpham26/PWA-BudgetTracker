fdfaeb92a7ffec97636d6d6952b724ff48432383
unknown <vulong.pham26@gmail.com>
2022-04-30 13:13:40 -0400

    fixed server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget", 
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
