0619b4cc3e9e9492817d38c8231801e3fb1551a2
unknown <vulong.pham26@gmail.com>
2022-05-01 19:23:28 -0400

    trying to fix everythign

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget"
 const app = express();
 
 app.use(logger("dev"));
@@ -15,8 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget", 
+mongoose.connect(MONGODB_URI,
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
